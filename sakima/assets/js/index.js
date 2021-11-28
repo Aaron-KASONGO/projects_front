189aebf69a0e915f60515cc7c4b9554a8f19518a
Aaron KASONGO <19kk181@esisalama.org>
2021-11-28 16:33:09 +0100

    final page

--- sakima/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $( "a.link" ).toggle(
-        function() {
-          $( this ).addClass( "active" );
-        }, function() {
-          $( this ).removeClass( "active" );
-        }
-      );
-})
