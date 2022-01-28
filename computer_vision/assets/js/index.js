ce2afff6bd9f0c2095f64a7abd2f45b4cef0d120
Aaron KASONGO <19kk181@esisalama.org>
2022-01-28 22:47:14 +0100

    Adding of beautiful and responsive uploading file input

---
+++ computer_vision/assets/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('#file').change(function(){
+        const file = this.files[0];
+        console.log(file);
+        if (file){
+          let reader = new FileReader();
+          reader.onload = function(event){
+            console.log(event.target.result);
+            $('#id-image').attr('src', event.target.result);
+            $('#ig-image').attr('src', event.target.result);
+            $('#id-div').removeClass('d-none');
+          }
+          reader.readAsDataURL(file);
+        }
+    })
+
+    $('#id-button').click(function() {
+      $('#file').click();
+    })
+})
